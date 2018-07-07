3908fa14dd835e3be839d237045339cc5810b5e0
Drake Fish <drakefish@drakes-mbp.attlocal.net>
2018-07-07 11:12:32 -0500

    completed deal, print, and save to file functions for cards

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	cards := newDeck()
+
+	// 	hand, remainingDeck := deal(cards, 5)
+
+	// 	hand.print()
+	// 	remainingDeck.print()
+	// }
+
+	// func newCard() string {
+	// 	return "Five of Diamonds"
+	cards.saveToFile("my_cards")
+}
